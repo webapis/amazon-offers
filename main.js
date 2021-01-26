f53cf2370807325d552d98c6316089c0db3582d4
webapis <webapis.github@gmail.com>
2021-01-26 09:37:11 +0500

    git intagration

--- main.js
+++ main.js
@@ -1,26 +1,105 @@
-// This is the main Node.js source code file of your actor.
-// It is referenced from the "scripts" section of the package.json file,
-// so that it can be started by running "npm start".
-
 const Apify = require('apify');
 
 Apify.main(async () => {
-    // Get input of the actor.
-    // If you'd like to have your input checked and generate a user interface
-    // for it, add INPUT_SCHEMA.json file to your actor.
-    // For more information, see https://apify.com/docs/actor/input-schema
-    const input = await Apify.getInput();
-    console.log('Input:');
-    console.dir(input);
+  const { keyword } = await Apify.getInput();
 
-    // Do something useful here...
+  const requestQueue = await Apify.openRequestQueue();
+  await requestQueue.addRequest({
+    url: `https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=${keyword}`,
+  });
 
-    // Save output
-    const output = {
-        receivedInput: input,
-        message: 'Hello sir!',
-    };
-    console.log('Output:');
-    console.dir(output);
-    await Apify.setValue('OUTPUT', output);
+  const handlePageFunction = async ({ request, page }) => {
+    if (!request.userData.detailPage && !request.userData.offerPage) {
+      let asins = await page.$$eval('div[data-asin]', (els) =>
+        els
+          //.filter((e, i) => i < 20)
+          .map((el) => el.getAttribute('data-asin'))
+      );
+      asins.forEach((ASIN) => {
+        if (ASIN !== '') {
+          const detailRequest = new Apify.Request({
+            url: `https://www.amazon.com/dp/${ASIN}`,
+            userData: { detailPage: true, ASIN },
+          });
+          requestQueue.addRequest(detailRequest);
+        }
+      });
+    } else if (request.userData.detailPage) {
+      const { ASIN } = request.userData;
+      let title = await page.$eval('#title', (el) => el.innerText.trim());
+      let url = request.url;
+
+      let descriptionExist = await page.$('#productDescription');
+      let description = null;
+      if (descriptionExist) {
+        description = await page.$eval('#productDescription', (el) =>
+          el.innerText
+            .replace(/(\r\n\t|\n|\r|\t)/gm, '')
+            .replace(/^\s+|\s+$/g, '')
+        );
+      } else {
+        description = await page.$eval('#aplus', (el) =>
+          el.innerText
+            .replace(/(\r\n\t|\n|\r|\t)/gm, '')
+            .replace(/^\s+|\s+$/g, '')
+        );
+      }
+
+      const offerRequest = new Apify.Request({
+        url: `https://www.amazon.com/gp/offer-listing/${ASIN}`,
+        userData: { offerPage: true, title, url, description },
+      });
+      requestQueue.addRequest(offerRequest);
+
+      console.log('detail page', title);
+    } else if (request.userData.offerPage) {
+      const { title, url, description } = request.userData;
+      console.log('offer Page', title);
+      //OFFERS
+      const data = await page.$$eval(
+        '.olpOffer',
+        ($offers, _title, _url, _keyword, _description) => {
+          const scrapedOffers = [];
+
+          return (
+            $offers
+              // .filter((o, i) => i < 20)
+              .map(($offer) => {
+                return {
+                  title: _title,
+                  url: _url,
+                  description: _description,
+                  keyword: _keyword,
+                  sellerName: $offer
+                    .querySelector('.olpDeliveryColumn')
+                    .innerText.replace(/(\r\n\t|\n|\r|\t)/gm, '')
+                    .replace(/^\s+|\s+$/g, ''),
+                  pricePlusShipping: $offer.querySelector('.olpPriceColumn')
+                    .innerText,
+                };
+              })
+          );
+        },
+        title,
+        url,
+        keyword,
+        description
+      );
+
+      const offersDataSet = await Apify.openDataset('offers');
+      await offersDataSet.pushData({ data });
+    }
+  };
+   //const proxyConfiguration = await Apify.createProxyConfiguration();
+  const crawler = new Apify.PuppeteerCrawler({
+   // maxRequestsPerCrawl: 20,
+    requestQueue,
+    //proxyConfiguration,
+    handlePageFunction,
+    launchPuppeteerOptions: {
+      headless: true,
+    },
+  });
+  await crawler.run();
+
 });
