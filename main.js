36a809b2d6ad9e67512e26e3335dd099c89103ba
webapis <webapis.github@gmail.com>
2021-02-09 11:44:23 +0500

    v

--- main.js
+++ main.js
@@ -34,5 +34,5 @@ Apify.main(async () => {
     throw error;
   }
 });
-
+//
 ///Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
